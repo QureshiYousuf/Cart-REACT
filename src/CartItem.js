edbb47e5b83a981687085010c026a4b07c1de14f
QureshiYousuf <yousuf337692qureshi@gmail.com>
2023-08-09 06:14:48 +0530

    Created Cart Item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,48 @@
 import React from "react";
 
+// installing fontawesome
+// npm i --save @fortawesome/fontawesome-svg-core 
+// npm i --save @fortawesome/free-solid-svg-icons    
+// npm i --save @fortawesome/free-regular-svg-icons 
+// npm i --save @fortawesome/react-fontawesome@latest
+
+// using fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, faTwitter, faFontAwesome);
+
+
 class CartItem extends React.Component {
+
+    constructor () {
+        super();
+        this.state = {
+            title: 'IPhone',
+            price: 999,
+            qty: 1,
+            img: ''
+        }
+    }
+
     render() {
+        // object destructuring
+        const {title, price, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25, fontWeight: "bold"}}>Phone</div>
-                    <div style={{fontSize: 15, color: "grey"}}>Rs 999</div>
-                    <div style={{fontSize: 15, color: "grey"}}>Qty: 1</div>
+                    <div style={{fontSize: 25, fontWeight: "bold"}}> {title} </div>
+                    <div style={{fontSize: 15, color: "grey"}}> {price} </div>
+                    <div style={{fontSize: 15, color: "grey"}}> {qty} </div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <h4 className="action-icons"><FontAwesomeIcon icon="fa-solid fa-cart-plus" /></h4>
+                        <h4 className="action-icons"><FontAwesomeIcon icon="fa-solid fa-square-minus" /></h4>
+                        <h4 className="action-icons"><FontAwesomeIcon icon="fa-solid fa-trash" /></h4>
                     </div>
                 </div>
             </div>
